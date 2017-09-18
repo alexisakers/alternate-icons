e8ec267b753c4159c56e0e79aaf4b604da3d58bc
Alexis Aubry <me@alexaubry.fr>
2017-09-18 16:57:53 +0200

    Migrate package tools to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "AlternateIcons",
-    targets: [
-        Target(name: "AltKit"),
-        Target(name: "AlternateIcons", dependencies: ["AltKit"])
-    ],
     dependencies: [
-        .Package(url: "https://github.com/JohnSundell/Files.git", majorVersion: 1),
-        .Package(url: "https://github.com/JohnSundell/Unbox.git", majorVersion: 2)
+        .package(url: "https://github.com/JohnSundell/Files.git", from: "1.11.0")
+    ],
+    targets: [
+        .target(name: "AltKit", dependencies: ["Files"]),
+        .target(name: "AlternateIcons", dependencies: ["AltKit"]),
+        .testTarget(name: "AltKitTests", dependencies: ["AltKit"])
     ]
 )
